a9c4947926028f4da89100224949d0102da23cb9
Christopher Becker <chrisb611@gmail.com>
2013-11-25 22:09:31 +0100

    Added config.dist

--- Module.php
+++ Module.php
@@ -2,10 +2,9 @@
 
 namespace EventErrorLogger;
 
-use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\Mvc\MvcEvent;
 
-class Module implements ConfigProviderInterface
+class Module
 {
     public function onBootstrap(MvcEvent $e)
     {
@@ -21,17 +20,28 @@ class Module implements ConfigProviderInterface
         $serviceManager = $application->getServiceManager();
         $config         = $serviceManager->get('config');
 
-        if($config['eventerrorlogger']['log'] == false){
+        if(isset($config['eventerrorlogger']) == false){
             return;
         }
 
-        foreach($config['eventerrorlogger']['loggers'] as $loggerName){
+        $config = $config['eventerrorlogger'];
+
+        if($config['log'] == false){
+            return;
+        }
+
+        foreach($config['loggers'] as $loggerName){
             if($serviceManager->has($loggerName) == false){
                 continue;
             }
 
-            $routeMatch = $e->getRouteMatch();
             $logger = $serviceManager->get($loggerName);
+
+            if($logger instanceof \Zend\Log\LoggerInterface == false){
+                continue;
+            }
+
+            $routeMatch = $e->getRouteMatch();
 
             if(isset($routeMatch)){
                 $logger->crit($routeMatch->getMatchedRouteName());
@@ -45,17 +55,6 @@ class Module implements ConfigProviderInterface
 
     public function getConfig()
     {
-        return include __DIR__ . '/config/module.config.php';
-    }
-
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
+        return include __DIR__ . '/config/eventerrorlogger.global.php.dist';
     }
 }
--- README.md
+++ README.md
@@ -2,7 +2,17 @@ EventErrorLogger
 ================
 
 This module logs dispatch.error and render.error events.
+It's useful on a production system to get notified when a user get a error page.
 
 ##Installation
 - Add "weteef/eventerrorlogger": "1.*" to the require section of your composer.json
 - Add ```'EventErrorLogger'``` to the modules array in your application.config.php
+- Copy the file ```config/eventerrorlogger.global.php.dist``` to the ```config/autoload```
+folder an rename it to ```eventerrorlogger.global.php```
+- Add a logger to the loggers array. Example:
+```
+'loggers' => array(
+      'myLogger'
+),
+```
+The logger need to be a instance of \Zend\Log\LoggerInterface
